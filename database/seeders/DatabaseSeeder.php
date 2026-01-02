6390cf814edb42051babac1bcefae2d7d1701fcb
Gresia Maryance Batuara <gresiamryance@gmail.com>
2026-01-02 21:02:22 +0700

    Update seeder and env example

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,19 +3,20 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use App\Models\Kontak; // Pastikan import model ini
+use App\Models\Kontak;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        // Data awal untuk tabel kontak
+        // Jalankan UserSeeder (INI YANG PENTING)
+        $this->call(UserSeeder::class);
+
+        // Data awal kontak sekolah
         Kontak::create([
             'email' => 'admin@smpbudimulia.sch.id',
             'no_telp' => '+62 812-xxxx-xxxx',
             'alamat' => 'Jl. Pelajar, Pangururan, Kabupaten Samosir, Sumatera Utara'
         ]);
-        
-        // Anda juga bisa menambahkan seeder untuk User Admin di sini jika belum ada
     }
 }
