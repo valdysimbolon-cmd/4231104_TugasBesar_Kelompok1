7bc123b35136cf99b07ddcb316294dfe5d93b283
Valdy Simbolon <valdysimbolon@gmail.com>
2026-01-03 19:56:56 +0700

    ada yang kurang tadi di gambar

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,20 +3,19 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use App\Models\Kontak;
+use App\Models\Kontak; // Pastikan import model ini
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        // Jalankan UserSeeder (INI YANG PENTING)
-        $this->call(UserSeeder::class);
-
-        // Data awal kontak sekolah
+        // Data awal untuk tabel kontak
         Kontak::create([
             'email' => 'admin@smpbudimulia.sch.id',
             'no_telp' => '+62 812-xxxx-xxxx',
             'alamat' => 'Jl. Pelajar, Pangururan, Kabupaten Samosir, Sumatera Utara'
         ]);
+        
+        // Anda juga bisa menambahkan seeder untuk User Admin di sini jika belum ada
     }
 }
