0ff7f5b3f8c84845039b72cfc86e80d7f04b8140
Valdy Simbolon <valdysimbolon@gmail.com>
2026-01-04 21:15:59 +0700

    gambar

--- resources/views/guest/sections/hero.blade.php
+++ resources/views/guest/sections/hero.blade.php
@@ -1,10 +1,8 @@
-<header class="masthead">
+<header class="masthead" style="background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url('{{ asset('Admin/img/profil/sekolah-bg.jpg') }}') !important; background-size: cover !important; background-position: center !important;">
     <div class="container text-center text-white">
         <div class="masthead-subheading">Selamat Datang di Portal Resmi</div>
         <div class="masthead-heading text-uppercase">SMP Budi Mulia Pangururan</div>
-        <a class="btn btn-primary btn-xl text-uppercase px-5 shadow"
-           style="background-color:#0d6efd; border-color:#0d6efd;"
-           href="#services">
+        <a class="btn btn-primary btn-xl text-uppercase px-5 shadow" href="#services">
            Kenali Kami Lebih Dekat
         </a>
     </div>
